6e657a386a2b6e23b178f1b21ed45cfe723bc718
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2015-12-07 01:21:04 -0200

    Adicionando dao para controle de Usuario

---
+++ dao/UsuarioDao.php
@@ -0,0 +1,67 @@
+<?php
+
+class UsuarioDao {
+
+  /**
+   * @var Banco
+   */
+  private $oBanco;
+
+  public function __construct() {
+    $this->oBanco = new MySQLBanco();
+  }
+
+  public function validarUsuario(Usuario $oUsuario) {
+
+    $this->oBanco->conectar();
+
+    $sUsuario = $this->oBanco->escapeStrings($oUsuario->getUsuario());
+    $sSenha   = $oUsuario->getSenha();
+
+    $sSql = " select id from usuario where usuario = '{$sUsuario}'  and senha = '{$sSenha}';";
+
+    $rsUsuario = $this->oBanco->query($sSql);
+
+    if ($rsUsuario === false) {
+      die($sSql);
+    }
+
+    $iLinhas = $this->oBanco->numeroLinhas($rsUsuario);
+
+    if ($iLinhas != 1) {
+        return false;
+    }
+
+      $oStdUsuario = $this->oBanco->getResgitro($rsUsuario, 0);
+      $oUsuario->setCodigo($oStdUsuario->id);
+      return true;
+  }
+
+  public function buscarPorCodigo($iCodigo) {
+
+      $this->oBanco->conectar();
+      $iCodigo = $this->oBanco->escapeStrings($iCodigo);
+
+      $sSql = " select * from usuario where id = {$iCodigo} ;";
+
+      $rsUsuario = $this->oBanco->query($sSql);
+
+      if ($rsUsuario === false) {
+          die($sSql);
+      }
+
+      $iLinhas = $this->oBanco->numeroLinhas($rsUsuario);
+
+      if ($iLinhas != 1) {
+          die("Erro ao buscar usuário");
+      }
+
+      $oStdUsuario = $this->oBanco->getResgitro($rsUsuario, 0);
+
+      $oUsuario = new Usuario();
+      $oUsuario->setCodigo($oStdUsuario->id);
+      $oUsuario->setUsuario($oStdUsuario->usuario);
+
+      return $oUsuario;
+  }
+}
