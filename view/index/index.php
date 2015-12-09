307c629ffd08dc6e15548111c3ddbb19158069be
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2015-12-08 22:02:55 -0200

    Atualizando mensagens da home com novo recurso.

--- view/index/index.php
+++ view/index/index.php
@@ -1,18 +1,23 @@
 <h1>Bem vindo ao sistema petshop web</h1>
 
 <p class="alert alert-success">
+    08/12/2015 22:00 - Adicionado campo (checkbox) para informar se o animal possui pacote (cliente pacote) no cadastro
+    de animal. Alguns ajustes no leiaute.
+</p>
+
+<p class="alert alert-warning">
     08/12/2015 01:15 - Ajuste no Cadastro: campo peso agora aceita somente valores com vírgula (40,5 por exemplo) e campo
     data aceita valores no formato dd/mm/aaaa e aaaa-mm-dd.
 </p>
 
-<p class="alert alert-success">
+<p class="alert alert-warning">
     07/12/2015 21:55 - Removida a obrigatoriedade do campo Observações no cadastro de Animal.
 </p>
 
-<p class="alert alert-success">
+<p class="alert alert-warning">
     07/12/2015 21:40 - Removendo menus que ainda não estão prontos.
 </p>
 
-<p class="alert alert-success">
+<p class="alert alert-warning">
     06/12/2015 00:01 - Lançamento do sistema Meu Pet.
 </p>
