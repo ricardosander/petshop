7a81bd56c55dcb32f8af7a74a8044848b12ac24c
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2016-02-25 01:08:46 -0300

    Melhorando paginacao

--- classes/PaginacaoSimples.php
+++ classes/PaginacaoSimples.php
@@ -2,93 +2,155 @@
 
 class PaginacaoSimples implements Paginacao {
 
-    /**
-     * @var int Número de registros por página.
-     */
-    private $iRegistrosPorPagina;
+  /**
+   * @var int Número de registros por página.
+   */
+  private $iRegistrosPorPagina;
 
-    /**
-     * @var int Número total de registros.
-     */
-    private $iTotalRegistros;
+  /**
+   * @var int Número total de registros.
+   */
+  private $iTotalRegistros;
 
-    /**
-     * @var int Página atual.
-     */
-    private $iPagina;
+  /**
+   * @var int Página atual.
+   */
+  private $iPagina;
 
-    /**
-     * Parâmetros
-     * @var array
-     */
-    private $aParametros;
+  /**
+   * Parâmetros
+   * @var array
+   */
+  private $aParametros;
 
-    public function __construct($sModulo, $iRegistrosPorPagina, $iTotalRegistros, $iPagina) {
+  /**
+   * @var string
+   */
+  private $sModulo;
 
-        $this->iRegistrosPorPagina = $iRegistrosPorPagina;
-        $this->iTotalRegistros     = $iTotalRegistros;
-        $this->iPagina             = $iPagina;
-        $this->sModulo             = $sModulo;
+  public function __construct($sModulo, $iRegistrosPorPagina, $iTotalRegistros, $iPagina) {
+
+    $this->iRegistrosPorPagina = $iRegistrosPorPagina;
+    $this->iTotalRegistros = $iTotalRegistros;
+    $this->iPagina = $iPagina;
+    $this->sModulo = $sModulo;
+  }
+
+  public function getOffset() {
+    return ($this->iPagina - 1) * $this->iRegistrosPorPagina;
+  }
+
+  public function getLimit() {
+    return $this->iRegistrosPorPagina;
+  }
+
+  public function getPaginacao() {
+
+    if ($this->iTotalRegistros <= $this->iRegistrosPorPagina) {
+      return "";
     }
 
-    public function getOffset() {
-        return ($this->iPagina - 1) * $this->iRegistrosPorPagina;
+    $sParametros = "";
+    if (!empty($this->aParametros)) {
+
+      $aParametrosAux = array();
+      $aParametrosAux[] = "busca=1";
+      foreach ($this->aParametros as $sChave => $sValor) {
+        $aParametrosAux[] = "{$sChave}={$sValor}";
+      }
+      $sParametros = "?" . implode("&", $aParametrosAux);
     }
 
-    public function getLimit() {
-        return $this->iRegistrosPorPagina;
+    $iTotalPaginas = intval($this->iTotalRegistros / $this->iRegistrosPorPagina);
+
+    if ($this->iTotalRegistros % $this->iRegistrosPorPagina > 0) {
+      $iTotalPaginas++;
     }
 
-    public function getPaginacao() {
+    $sHtml = "<p class='text-info'>Página {$this->iPagina}/{$iTotalPaginas}</p>";
+    $sHtml .= "<ul class='pagination'>";
 
-        if ($this->iTotalRegistros <= $this->iRegistrosPorPagina) {
-            return "";
+    //Cálcula a página de inicio da paginação.
+    $iInicio = $this->iPagina - 2;
+    if ($iInicio < 1) {
+      $iInicio = 1;
+    }
+
+    //Cálcula a página de fim da paginação.
+    $iFim = $this->iPagina + 2;
+    if ($iFim > $iTotalPaginas) {
+      $iFim = $iTotalPaginas;
+    }
+
+    //Ajusta a diferença para sempre termos 5 páginas sequenciais.
+    if (($iFim - $iInicio) != 4) {
+
+      $iDifenca = 4 - ($iFim - $iInicio);
+      if ($iInicio == 1) {
+        $iFim += $iDifenca;
+      }
+
+      if ($iFim == $iTotalPaginas) {
+        $iInicio -= $iDifenca;
+      }
+    }
+
+    if ($iInicio < 1) {
+      $iInicio = 1;
+    }
+
+    if ($iFim > $iTotalPaginas) {
+      $iFim = $iTotalPaginas;
+    }
+
+    for ($i = $iInicio; $i <= $iFim; $i++) {
+
+      $sCaminho = "/{$this->sModulo}/lista";
+      $sLink    = "{$sCaminho}/{$i}";
+      $sClass   = "";
+
+      if ($i == $iInicio && $i != $this->iPagina && $iInicio != 1) {
+        $sHtml .= $this->montaLinkPaginacao(1, "{$sCaminho}/1", $sParametros);
+
+        if (($i - 1) != 1) {
+          $sHtml .= $this->montaLinkPaginacao("...", "");
         }
+      }
 
-        $sParametros = "";
-        if (!empty($this->aParametros)) {
+      if ($i == $this->iPagina) {
 
-            $aParametrosAux   = array();
-            $aParametrosAux[] = "busca=1";
-            foreach ($this->aParametros as $sChave => $sValor) {
-                $aParametrosAux[] = "{$sChave}={$sValor}";
-            }
-            $sParametros = "?" . implode("&", $aParametrosAux);
+        $sLink = "#";
+        $sClass = "class='active'";
+      }
+
+      $sHtml .= $this->montaLinkPaginacao($i, $sLink, $sParametros, $sClass);
+
+      if ($i == $iFim && $iFim != $iTotalPaginas) {
+
+        if (($i + 1) != $iTotalPaginas) {
+          $sHtml .= $this->montaLinkPaginacao("...", "");
         }
-
-        $iTotalPaginas = intval($this->iTotalRegistros / $this->iRegistrosPorPagina);
-
-        if ($this->iTotalRegistros % $this->iRegistrosPorPagina > 0) {
-            $iTotalPaginas++;
-        }
-
-        $sHtml  = "<p class='text-info'>Página {$this->iPagina}/{$iTotalPaginas}</p>";
-        $sHtml .= "<ul class='pagination'>";
-
-
-        for ($i = 1; $i <= $iTotalPaginas; $i++) {
-
-            $sLink  = "/{$this->sModulo}/lista/{$i}";
-            $sClass = "";
-
-            if ($i == $this->iPagina) {
-
-                $sLink  = "#";
-                $sClass = "class='active'";
-            }
-
-            $sHtml .= "<li {$sClass} ><a href='{$sLink}{$sParametros}'>{$i}</a></li>";
-        }
-
-        $sHtml .= "</ul><br>";
-        return $sHtml;
+        $sHtml .= $this->montaLinkPaginacao($iTotalPaginas, "{$sCaminho}/{$iTotalPaginas}", $sParametros);
+      }
     }
 
-    public function getSql() {
-        return " limit {$this->getLimit()} offset {$this->getOffset()} ";
-    }
+    $sHtml .= "</ul><br>({$this->iTotalRegistros} resultados)<br><br>";
+    return $sHtml;
+  }
 
-    public function setParametros($aParametros) {
-        $this->aParametros = $aParametros;
+  private function montaLinkPaginacao($iPosicao, $sLink, $sParametros = "", $sClass = "") {
+
+    if (!empty($sLink)) {
+      $sLink = "href='{$sLink}{$sParametros}'";
     }
+    return "<li {$sClass} ><a {$sLink}>{$iPosicao}</a></li>";
+  }
+
+  public function getSql() {
+    return " limit {$this->getLimit()} offset {$this->getOffset()} ";
+  }
+
+  public function setParametros($aParametros) {
+    $this->aParametros = $aParametros;
+  }
 }
