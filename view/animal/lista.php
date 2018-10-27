098fcb72f720d4c2798f0e8d9c3e594f3806ba1b
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2018-10-27 03:36:41 -0300

    Testes com Angular + API.

--- view/animal/lista.php
+++ view/animal/lista.php
@@ -1,3 +1,46 @@
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular-cookies.js"></script>
+<script>
+    var app = angular.module("myApp", ['ngCookies']);
+    app.controller("pets", function ($scope, $http, $cookies) {
+
+        $scope.pets = [];
+
+        console.log($cookies.getAll());
+
+        const config = {
+            headers: {
+                'skey': $cookies.get("skey")
+            }
+        };
+
+        $scope.show_no_pets = false;
+        $scope.show_pets = false;
+
+        $http.get("http://localhost:8080/pets", config)
+            .then(function (response) {
+
+                    if (response.status === 200) {
+
+                        var data = response.data;
+
+                        $scope.show_no_pets = data.length === 0;
+                        $scope.show_pets = data.length > 0;
+
+                        for (var i = 0; i < data.length; i++) {
+                            console.log(data[i]);
+                            $scope.pets.push(data[i]);
+                        }
+                    }
+                }, function (response) {
+                    console.log(response);
+                    console.log(response.status + " - " + response.statusText + " : " + response.data);
+                    alert("Problemas ao carregar a página. Tente novamente.");
+                }
+            );
+
+    });
+</script>
 <div class="col-lg-4">
     <form method="get" action="/animal/lista">
         <fieldset>
@@ -5,18 +48,19 @@
             <div class="form-group row">
                 <label class="col-sm-2 form-control-label" for="nomeBusca">Nome:</label>
                 <div class="col-sm-10">
-                    <input type="text" name="nomeBusca" id="nomeBusca" class="form-control form-group" value="<?= isset($sBuscaNome) ? $sBuscaNome : "" ?>">
+                    <input type="text" name="nomeBusca" id="nomeBusca" class="form-control form-group"
+                           value="<?= isset($sBuscaNome) ? $sBuscaNome : "" ?>">
                 </div>
             </div>
-            <input type="hidden" name="busca" value="1" />
+            <input type="hidden" name="busca" value="1"/>
             <button type="submit" class="btn btn-primary">Buscar</button>
         </fieldset>
     </form>
 </div>
 
 
-<div class="col-sm-12">
-    <?php if(count($aAnimais) > 0)  { ?>
+<div class="col-sm-12" ng-app="myApp" ng-controller="pets">
+    <div ng-show="show_pets">
         <h1>Lista de Animais</h1>
         <table class="table table-bordered table-striped table-hover">
             <tr>
@@ -26,39 +70,27 @@
                 <td>Raça</td>
                 <td>Ações</td>
             </tr>
-            <?php
-            foreach ($aAnimais as $oAnimal) {
-                ?>
-                <tr>
-                    <td>
-                        <?= $oAnimal->getNome() ?>
-                    </td>
-                    <td>
-                        <?= empty($oAnimal->getCliente()) ? "" : $oAnimal->getCliente()->getNome() ?>
-                    </td>
-                    <td>
-                        <?= $oAnimal->getEspecie() ?>
-                    </td>
-                    <td>
-                        <?= $oAnimal->getRaca() ?>
-                    </td>
-                    <td>
-                        <?php if ($selecao) { ?>
-                            <a class="btn btn-default" href="/animal/seleciona/<?= $oAnimal->getCodigo() ?>/<?= $vinculo ?>/<?= $codigoVinculo ?>">Selecionar</a>
-                        <?php } else { ?>
-                            <a class="btn btn-success" href="/animal/ver/<?= $oAnimal->getCodigo() ?>">Ver</a>
-                            <a class="btn btn-primary" href="/animal/editar/<?= $oAnimal->getCodigo() ?>">Editar</a>
-                            <a class="btn btn-danger" href="/animal/excluir/<?= $oAnimal->getCodigo() ?>">Excluir</a>
-                        <?php } ?>
-                    </td>
-                </tr>
-                <?php
-            }
-            ?>
+            <tr ng-repeat="pet in pets">
+                <td>{{pet.name}}</td>
+                <td>{{pet.owner}}</td>
+                <td>{{pet.species}}</td>
+                <td>{{pet.breed}}</td>
+                <td>
+                    <?php if ($selecao) { ?>
+                        <a class="btn btn-default"
+                           href="/animal/seleciona/{{pet.id}}/<?= $vinculo ?>/<?= $codigoVinculo ?>">Selecionar</a>
+                    <?php } else { ?>
+                        <a class="btn btn-success" href="/animal/ver/{{pet.id}}">Ver</a>
+                        <a class="btn btn-primary" href="/animal/editar/{{pet.id}}">Editar</a>
+                        <a class="btn btn-danger" href="/animal/excluir/{{pet.id}}">Excluir</a>
+                    <?php } ?>
+                </td>
+            </tr>
         </table>
         <?= isset($oPaginacao) ? $oPaginacao->getPaginacao() : "" ?>
-    <?php } else { ?>
+    </div>
+    <div ng-show="show_no_pets">
         <p class="text-info">Nenhum animal encontrado!</p>
-    <?php } ?>
+    </div>
     <a class="btn btn-primary" href="/animal/lista">Voltar a lista completa</a>
 </div>
